1b36b227b499401bd710c0c2ea234a5a4868a147
mimosa <tim.beech@gmail.com>
2013-12-12 13:48:27 -0300

    add search for similar packages where no match

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@
 
 chmod +x pkgdepcheck
 chmod +x src.sh
+chmod +x slapt-get14.0
+chmod +x slapt-get-update
 mkdir -p /usr/libexec/pkgdepcheck
 cp ./src.sh /usr/libexec/pkgdepcheck
+cp ./slapt-get14.0 /usr/libexec/pkgdepcheck
+cp ./slapt-get-update /usr/libexec/pkgdepcheck
 cp ./pkgdepcheck /usr/bin
+mkdir -p /var/slapt-get14.0
+cp slapt-getrc14.0 /etc/slapt-get
