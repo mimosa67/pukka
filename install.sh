f3eef7869e1103d4313061dc65619ae17724818f
mimosa <tim.beech@gmail.com>
2013-12-21 16:14:36 -0300

    Debugged version ready for testing

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 
 chmod +x pukka slkparse.sh slapt-get-search.sh slapt-get-update.sh
 mkdir -p /usr/libexec/pkgdepcheck
-cp slkparse.sh slapt-get-search.sh slapt-get-update.sh /usr/libexec/pkgdepcheck
+cp pkgdepcheckrc slkparse.sh slapt-get-search.sh slapt-get-update.sh /usr/libexec/pkgdepcheck
 cp ./pukka /usr/local/bin
